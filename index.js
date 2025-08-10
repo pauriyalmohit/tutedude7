fd8f3aaca876531ebcd79b80bbc384c4b855f20f
pauriyalmohit <161916772+pauriyalmohit@users.noreply.github.com>
2025-08-10 23:56:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+ 
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+let items = [];
+
+app.get("/", (req, res) => {
+  res.render("list", { ejes: items }); // pass ejes here
+});
+
+app.post("/", (req, res) => {
+  const newItem = req.body.elel;
+  if (newItem && newItem.trim() !== "") {
+    items.push(newItem); // push the new task
+  }
+  res.redirect("/"); // will trigger GET above, sending ejes
+});
+
+app.listen(8000, () => {
+  console.log("Server stared");
+});
